9e09226d4c8ca8a45b7f0c00e70893164360c6f9
bandstrar <rob.bandstra@gmail.com>
2021-05-27 21:15:17 -0500

    added group repo and controller

---
+++ GameNight/Controllers/GameGroupsController.cs
@@ -0,0 +1,66 @@
+﻿using GameNight.DataAccess;
+using GameNight.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.Controllers
+{
+    [Route("api/groups")]
+    [ApiController]
+    public class GameGroupsController : ControllerBase
+    {
+        GameGroupsRepository _repo;
+
+        public GameGroupsController()
+        {
+            _repo = new GameGroupsRepository();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGroups()
+        {
+            return Ok(_repo.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var group = _repo.Get(id);
+
+            if (group == null)
+            {
+                return NotFound("This group id does not exist");
+            }
+
+            return Ok(group);
+        }
+
+        [HttpPost]
+        public IActionResult AddUser(GameGroup group)
+        {
+            _repo.Add(group);
+
+            return Created($"api/SiteUsers/{group.Id}", group);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(GameGroup group)
+        {
+            _repo.Update(group);
+
+            return Ok(group);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGroup(int id)
+        {
+            _repo.Remove(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ GameNight/DataAccess/GameGroupsRepository.cs
@@ -0,0 +1,75 @@
+﻿using Dapper;
+using GameNight.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.DataAccess
+{
+    public class GameGroupsRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=GameNight;Trusted_Connection=True;";
+
+        public List<GameGroup> GetAll()
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from GameGroup";
+
+            return db.Query<GameGroup>(sql).ToList();
+        }
+
+        public IEnumerable<GameGroup> Get(string id)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"select *
+                        from GameGroup
+                        where id = @id";
+
+            var group = db.Query<GameGroup>(sql, new { id });
+
+            return group;
+        }
+
+        public void Add(GameGroup group)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"INSERT INTO [GameGroup] ([Name],[Image],[Description])
+                            OUTPUT inserted.id
+                            VALUES(@Name,@Image,@Description)";
+
+            var id = db.ExecuteScalar<int>(sql, group);
+
+            group.Id = id;
+        }
+
+        public void Update(GameGroup group)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"UPDATE GameGroup
+                        SET Name = @Name,
+                            Image = @Image,
+                            Description = @Description
+                        WHERE id = @id";
+
+            db.Execute(sql, group);
+        }
+
+        public void Remove(int id)
+        {
+            var sql = @"Delete 
+                        from GameGroup 
+                        Where id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            db.Execute(sql, new { id });
+        }
+    }
+}
