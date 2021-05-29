28571c6e5aae7f39d25cf06272dd31cb0b39b8a5
bandstrar <rob.bandstra@gmail.com>
2021-05-29 10:13:48 -0500

    added repo and controller for game nights

---
+++ GameNight/Controllers/GameNightsController.cs
@@ -0,0 +1,68 @@
+﻿using GameNight.DataAccess;
+using GameNight.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.Controllers
+{
+    [Route("api/gameNight")]
+    [ApiController]
+    public class GameNightsController : ControllerBase
+    {
+        GameNightsRepository _repo;
+
+        public GameNightsController(GameNightsRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGameNights()
+        {
+            var gameNights = _repo.GetAll();
+
+            return Ok(gameNights);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetNightById(int id)
+        {
+            var gameNight = _repo.GetById(id);
+
+            if (gameNight == null)
+            {
+                return NotFound("This game night id does not exist");
+            }
+
+            return Ok(gameNight);
+        }
+
+        [HttpPost]
+        public IActionResult AddGameNight(GameNights gameNight)
+        {
+            _repo.Add(gameNight);
+
+            return Created($"api/gameNight/{gameNight.Id}", gameNight);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateGameNight(GameNights gameNight)
+        {
+            _repo.Update(gameNight);
+
+            return Ok(gameNight);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGameNight(int id)
+        {
+            _repo.Remove(id);
+
+            return Ok();
+        }
+    }
+}
---
+++ GameNight/DataAccess/GameNightsRepository.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameNight.Models;
+using Microsoft.Data.SqlClient;
+using Dapper;
+
+namespace GameNight.DataAccess
+{
+    public class GameNightsRepository
+    {
+        readonly string ConnectionString;
+
+        public GameNightsRepository(IConfiguration config)
+        {
+            ConnectionString = config.GetConnectionString("GameNight");
+        }
+
+        public IEnumerable<GameNights> GetAll()
+        {
+            var sql = @"select * from GameNight gn
+	                        join GameGroup gg
+		                        on gg.id = gn.groupId";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var gameNights = db.Query<GameNights, GameGroup, GameNights>(sql,
+                (gameNight, gameGroup) =>
+                {
+                    gameNight.GameGroup = gameGroup;
+
+                    return gameNight;
+                }, splitOn: "Id");
+            return gameNights;
+        }
+
+        public IEnumerable<GameNights> GetById(int id)
+        {
+            var sql = @"select * from GameNight gn
+	                        join GameGroup gg
+		                        on gg.id = gn.groupId
+                        where gn.id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var gameNights = db.Query<GameNights, GameGroup, GameNights>(sql,
+                (gameNight, gameGroup) =>
+                {
+                    gameNight.GameGroup = gameGroup;
+
+                    return gameNight;
+                }, new { id });
+            return gameNights;
+        }
+
+        public void Add(GameNights gameNight)
+        {
+            var sql = @"INSERT INTO [GameNight] ([GroupId],[Title],[Date],[Description])
+                        OUTPUT inserted.id
+                        VALUES(@groupId, @title, @date, @description)";
+
+            using var db = new SqlConnection(ConnectionString);
+
+
+            var id = db.ExecuteScalar<int>(sql, gameNight);
+
+            gameNight.Id = id;
+        }
+
+        public void Update(GameNights gameNight)
+        {
+            using var db = new SqlConnection(ConnectionString);
+
+            var sql = @"UPDATE [GameNight]
+                        SET [GroupId] = @GroupId,
+                            [Title] = @Title,
+                            [Date] = @Date,
+                            [Description] = @Description
+                        WHERE id = @id";
+
+            db.Execute(sql, gameNight);
+        }
+
+        public void Remove(int id)
+        {
+            var sql = @"Delete 
+                        from GameNight 
+                        Where id = @id";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            db.Execute(sql, new { id });
+        }
+    }
+}
