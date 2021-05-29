6aba5bc3dce96f47319d1b6510e9358d39132bb4
bandstrar <rob.bandstra@gmail.com>
2021-05-28 19:30:41 -0500

    added repo and controller for group users

---
+++ GameNight/Controllers/GroupUsersController.cs
@@ -0,0 +1,84 @@
+﻿using GameNight.DataAccess;
+using GameNight.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameNight.Controllers
+{
+    [Route("api/groupUsers")]
+    [ApiController]
+    public class GroupUsersController : ControllerBase
+    {
+        GroupUsersRepository _repo;
+
+        public GroupUsersController(GroupUsersRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGroupUsers()
+        {
+            var groupUsers = _repo.GetAll();
+
+            return Ok(groupUsers);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetSingleGroupUser(int id)
+        {
+            var user = _repo.GetById(id);
+
+            if (user == null)
+            {
+                return NotFound("This group user id does not exist");
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult AddGroupUser(GroupUser groupUser)
+        {
+            _repo.Add(groupUser);
+
+            return Created($"api/GroupUsers/{groupUser.Id}", groupUser);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateGroupUser(GroupUser groupUser)
+        {
+            _repo.Update(groupUser);
+
+            return Ok(groupUser);
+        }
+
+        [HttpPut("{id}/makeInactive")]
+        public IActionResult DeactivateGroupUser(int id)
+        {
+            _repo.MakeInactive(id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/approve")]
+        public IActionResult ApproveGroupUser(int id)
+        {
+            _repo.ApproveUser(id);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteGroupUser(int id)
+        {
+            _repo.Remove(id);
+
+            return Ok();
+        }
+    }
+}
--- GameNight/Startup.cs
+++ GameNight/Startup.cs
@@ -47,6 +47,7 @@ namespace GameNight
             services.AddSingleton(Configuration);
             services.AddTransient<UsersRepository>();
             services.AddTransient<GameGroupsRepository>();
+            services.AddTransient<GroupUsersRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
