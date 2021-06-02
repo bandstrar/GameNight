8488690e0148c56354307a392672ddd8a008a142
bandstrar <rob.bandstra@gmail.com>
2021-06-01 20:47:20 -0500

    added buttons to find and create a group

--- GameNight/gamenight.ui/src/views/Groups.js
+++ GameNight/gamenight.ui/src/views/Groups.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'reactstrap';
 import groupData from '../helpers/data/groupData';
 import GroupCard from '../components/Cards/groupCard';
 
@@ -20,7 +21,20 @@ const Groups = (props) => {
 
   return (
     <div>
+      <div className="d-flex col-wrap justify-content-end">
+        <div className="mt-5">
+          <div>
       <h1>My Groups</h1>
+      </div>
+      <div className="group-button-container">
+      <Button className="mr-3">Find a Group</Button>
+      <Button>Create a Group</Button>
+      </div>
+      </div>
+      <div>
+        <img className="w-50 mt-5 mb-4" src={'https://i.imgur.com/HLoG7EM.jpg'} alt={'A group of young adults playing a board game.'} />
+      </div>
+      </div>
       <div className="group-card-container">
       {showGroups()}
       </div>
