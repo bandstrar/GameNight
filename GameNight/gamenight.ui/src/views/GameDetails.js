fe95ce28515249390629c1c5b385585e969eae08
bandstrar <rob.bandstra@gmail.com>
2021-06-12 13:47:54 -0500

    added switch statement for game weight

--- GameNight/gamenight.ui/src/views/GameDetails.js
+++ GameNight/gamenight.ui/src/views/GameDetails.js
@@ -24,22 +24,37 @@ const GameDetails = (props) => {
     return null;
   }
 
+  const getWeight = (weight) => {
+    switch (weight) {
+      case 0:
+        return 'Light';
+      case 1:
+        return 'Medium-Light';
+      case 2:
+        return 'Medium';
+      case 3:
+        return 'Medium-Heavy';
+      case 4:
+        return 'Heavy';
+      default:
+        return 'No Weight Assigned';
+    }
+  };
+
   return (
     <div>
-    <div className="game-detail-body">
-    <div className="d-flex col-wrap justify-content-center">
-      <div>
+    <div className="d-flex col-wrap game-detail-body">
+      <div className="mr-5 mt-3">
       <h1>{gameInfo.title}</h1>
       <h3>Player Count: {gameInfo.minPlayers}-{gameInfo.maxPlayers} Players</h3>
       <h3>Playing Time: {gameInfo.lengthInMinutes}</h3>
-      <h3>Game Weight: {gameInfo.weight}</h3>
+      <h3>Game Weight: {getWeight(gameInfo.weight)}</h3>
       <h3>Genre: {gameInfo.genre}</h3>
-      <Button onClick={() => gameProps.history.goBack()}>Back to My Games</Button>
+      <Button className="mt-3">Update Game</Button>
       </div>
-      <div>
+      <div className="ml-5">
         <img src={gameInfo.gameImage} alt={`${gameInfo.title} box art`} />
       </div>
-    </div>
     </div>
     </div>
   );
